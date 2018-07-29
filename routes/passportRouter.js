0c4457a6819f9d041038f5c3eff553f1786db3a4
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2018-07-29 15:05:22 -0400

    Minor edits

--- routes/passportRouter.js
+++ routes/passportRouter.js
@@ -1,8 +1,6 @@
 const express        	= require("express");
 const router         	= express.Router();
-// User model
 const User           	= require("../models/user");
-// Bcrypt to encrypt passwords
 const bcrypt         	= require("bcrypt");
 const bcryptSalt     	= 10;
 const ensureLogin 		= require("connect-ensure-login");
